54f03f575d3c70a333e03904e3161802557a3028
KRANTI PRAKASH <65568456+krantiprakash@users.noreply.github.com>
2025-03-26 01:04:54 +0530

    Add files via upload

---
+++ bostondynamics_spot_ws/spot_description/launch/spot_effort.launch.py
@@ -0,0 +1,62 @@
+import os
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import ExecuteProcess, RegisterEventHandler
+from launch.event_handlers import OnProcessStart, OnProcessExit
+from launch_ros.actions import Node
+import xacro
+
+
+def generate_launch_description():
+
+    spot_description_path = os.path.join(get_package_share_directory('spot_description'))
+
+    xacro_file = os.path.join(spot_description_path,'urdf','spot_effort.xacro.urdf')
+    doc = xacro.parse(open(xacro_file))
+    xacro.process_doc(doc)
+    robot_description = {'robot_description': doc.toxml()}
+
+    controller_config_file = os.path.join(spot_description_path, 'config', 'spot_control.yaml')
+
+    node_mujoco_ros2_control = Node(
+        package='mujoco_ros2_control',
+        executable='mujoco_ros2_control',
+        output='screen',
+        parameters=[robot_description, controller_config_file, {'mujoco_model_path':os.path.join(spot_description_path, 'model', 'spot_control.xml')}]
+    )
+
+    node_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[robot_description]
+    )
+    
+    load_joint_state_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'joint_state_broadcaster'],
+        output='screen'
+    )
+
+    load_joint_trajectory_controller = ExecuteProcess(
+        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
+             'effort_controller'],
+        output='screen'
+    )
+
+    return LaunchDescription([
+        RegisterEventHandler(
+            event_handler=OnProcessStart(
+                target_action=node_mujoco_ros2_control,
+                on_start=[load_joint_state_controller],
+            )
+        ),
+        RegisterEventHandler(
+            event_handler=OnProcessExit(
+                target_action=load_joint_state_controller,
+                on_exit=[load_joint_trajectory_controller],
+            )
+        ),
+        node_mujoco_ros2_control,
+        node_robot_state_publisher,
+    ])
